6c8066b4a8b91f58e4f8714a777aedd4ed1c6072
Gustavo Mendonça Zina <gustavomzina@gmail.com>
2026-03-12 14:35:32 -0400

    Initial setup

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	serveMux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+
+	server.ListenAndServe()
 }
