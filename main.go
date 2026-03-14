20e3b85ee91dafcf943c0d00e23227ba9ef01d6b
Gustavo Mendonça Zina <gustavomzina@gmail.com>
2026-03-14 19:52:39 -0400

    Add index.html

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	const port = "8080"
+
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
