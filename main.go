1aca299280d34f1555bfe6bb3e9d880a9315c448
Gustavo Mendonça Zina <gustavomzina@gmail.com>
2026-03-24 16:48:26 -0400

    add logo

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
