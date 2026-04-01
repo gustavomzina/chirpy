1f1777820471beec955435bbbe88aeb5bb859c0f
Gustavo Mendonça Zina <gustavomzina@gmail.com>
2026-04-01 10:09:28 -0400

    separates /app and /api routes

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ func main() {
 	const port = "8080"
 
 	apiConfig := &apiConfig{}
-	//	fileserverHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
-
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app", apiConfig.middlewareMetricsInc(http.HandlerFunc(handlerHello)))
-	//	serveMux.Handle("/app/", apiConfig.middlewareMetricsInc(fileserverHandler))
-	serveMux.HandleFunc("/healthz", handlerReadiness)
-	serveMux.Handle("/metrics", apiConfig.handlerMetrics())
-	serveMux.Handle("/reset", apiConfig.handlerResetMetrics())
+
+	fileserverHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	serveMux.Handle("GET /app/", apiConfig.middlewareMetricsInc(fileserverHandler))
+
+	serveMux.HandleFunc("GET /api/healthz", handlerReadiness)
+	serveMux.Handle("GET /api/metrics", apiConfig.handlerMetrics())
+	serveMux.Handle("POST /api/reset", apiConfig.handlerResetMetrics())
 
 	server := http.Server{
 		Addr:    ":" + port,
@@ -33,16 +33,6 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handlerHello(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-
-	_, err := w.Write([]byte("Welcome to Chirpy"))
 	if err != nil {
 		log.Fatal(err)
 	}
